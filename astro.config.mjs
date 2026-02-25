21c8fbc1ee0a25639134340730ca4d245f066de8
bizup-inc <suzuki@bizup-inc.co.jp>
2026-02-25 15:10:25 +0900

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,7 @@ import vercel from '@astrojs/vercel';
 
 const normalizedSiteUrl = process.env.SITE_URL
   ? process.env.SITE_URL.replace(/\/+$/, '')
-  : process.env.VERCEL_PROJECT_PRODUCTION_URL
-    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-    : undefined;
+  : 'https://insightbase.jp';
 
 // https://astro.build/config
 export default defineConfig({
